a26136d48f0fff06706c283481fce830b415d6e6
Ke Wang <453587854@qq.com>
2018-05-10 00:37:56 +0800

    点击获得用户身份

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { name: false, mobile: false, email: false, userInfo: null };
+    this.state = { name: false, mobile: false, email: false, userInfo: null, userIdentity: null };
   }
 
   checkbox(label, value) {
@@ -37,22 +37,42 @@ class App extends Component {
     }
   }
 
+  getUserIdentity() {
+    window.sys.getUserIdentity(data => {
+      this.setState({ userIdentity: data });
+    })
+  }
+
   render() {
     return <div>
-      <h1>
-        用户信息授权DEMO
-      </h1>
-      <div>
-        <h2>请求用户信息</h2>
-        {this.checkbox("用户名", "name")}
-        {this.checkbox("手机号", "mobile")}
-        {this.checkbox("邮箱", "email")}
-        <button onClick={() => this.doRequest()}>请求</button>
+      <section>
+        <h1>
+          用户信息授权DEMO
+        </h1>
+        <div>
+          <h2>请求用户信息</h2>
+          {this.checkbox("用户名", "name")}
+          {this.checkbox("手机号", "mobile")}
+          {this.checkbox("邮箱", "email")}
+          <button onClick={() => this.doRequest()}>请求</button>
 
-        <h2> 用户信息请求结果 </h2>
-        {!!this.state.userInfo ? <div>{JSON.stringify(this.state.userInfo)}</div> :
-          <div>(无结果)</div>}
-      </div>
+          <h2> 用户信息请求结果 </h2>
+          {!!this.state.userInfo ? <div>{JSON.stringify(this.state.userInfo)}</div> :
+            <div>(无结果)</div>}
+        </div>
+      </section>
+
+
+      <section>
+        <h1>
+          用户身份验证DEMO
+        </h1>
+        <button onClick={() => this.getUserIdentity()}>点击获得用户身份</button>
+        <h2> 用户身份验证结果 </h2>
+        {!!this.state.userIdentity ? <div>{JSON.stringify(this.state.userIdentity)}</div> :
+          <div>(未获得)</div>}
+      </section>
+
     </div>
   }
 }
