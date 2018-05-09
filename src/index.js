a6965b2cbaeddabea4dd0a173a5f5e685beb364e
Ke Wang <453587854@qq.com>
2018-05-10 00:15:42 +0800

    import a .jsx

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+const ReactDOM = require("react-dom");
+const React = require("react");
+import App from "./App.jsx";
+
+window.onload = function () {
+  let root = document.createElement("div");
+  document.body.appendChild(root);
+  ReactDOM.render(
+    React.createElement(App),
+    root);
+};
